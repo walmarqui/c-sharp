5e2cc2b05bc4eb5d296ffe061a932c3eeb96f59a
walmarqui <67659864+walmarqui@users.noreply.github.com>
2020-10-11 11:18:18 -0600

    agregando ejercicio de inventario

---
+++ ejercicios/inventario/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace inventario
+{
+    class Program
+    {
+
+        static string[,] productos = new string[5,3]
+        {
+            { "001", "Iphone X", "0"},
+            { "002", "Laptop Dell", "5"},
+            { "003", "Monitor Samsung", "2"},
+            { "004", "Mouse", "100"},
+            { "005", "Headset", "25"},
+        };
+        
+        static void listarProductos()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Listado de Productos");
+            Console.WriteLine("********************");
+            Console.WriteLine("Codigo, Descripcion, Existencia");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(productos[i,0] + " | " + productos[i,1] + " | " + productos[i,2]);
+            }
+            Console.ReadLine();
+        }
+
+        static void movimientoInventario(string codigo, int cantidad, string tipoMovimiento) {
+            for (int i = 0; i < 5; i++)
+            {
+                if (productos[i,0] == codigo) {
+                    if (tipoMovimiento == "+") {
+                        productos[i,2] = (Int32.Parse(productos[i,2]) + cantidad).ToString();
+                    } else {
+                        productos[i,2] = (Int32.Parse(productos[i,2]) - cantidad).ToString();
+                    }
+                }
+            }
+        }
+
+        static void ingresoDeInventario() {
+            string codigo = "";
+            string cantidad = "";
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Ingreso de Productos al Inventario");
+            Console.WriteLine("**********************************");
+            Console.Write("Ingrese el codigo del producto: ");
+            codigo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad del producto: ");
+            cantidad = Console.ReadLine();
+            
+            movimientoInventario(codigo, Int32.Parse(cantidad), "+");
+        }
+        static void Main(string[] args)
+        {
+            string opcion = "";
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Sistema de Inventario");
+                Console.WriteLine("*********************");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Productos");
+                Console.WriteLine("2 - Ingreso de Inventario");
+                Console.WriteLine("3 - Salida de Inventario");
+                Console.WriteLine("0 - Salir");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        listarProductos();
+                    break;
+                    case "2":
+                        ingresoDeInventario();
+                    break;
+                    default:
+                    break;
+                }  
+
+                if (opcion == "0")
+                {
+                    break;
+                }
+            }
+            
+        }
+    }
+}
